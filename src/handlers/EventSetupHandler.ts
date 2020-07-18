7ec5a2cb1271ceefcda30fe443ac30cb1295245f
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-07-18 17:57:04 +0300

    rework start cmd to work in temp channel on server

---
+++ src/handlers/EventSetupHandler.ts
@@ -0,0 +1,123 @@
+import { Message, GuildCreateChannelOptions, GuildChannel, TextChannel } from "discord.js"
+import { EventOptions } from "../entities/EventOptions"
+import { SetupStep } from "../entities/SetupStep"
+
+export class EventSetupHandler {
+	private eventQuestions: SetupStep[] = [
+		new SetupStep('What game would you like to play?', 'Game should be filled.', (response) => { return response && response !== "skip" ? true : false }, true),
+		new SetupStep('When? (send "skip" if you don\'t want to fill the field)', 'Error processing your response', (response) => { return response ? true : false }, false),
+		new SetupStep('How would you describe this event? (send "skip" if you don\'t want to fill the field)', 'Error processing your response', (response) => { return response ? true : false }, false)
+	]
+	
+	public async setupEvent(message: Message): Promise<EventOptions | undefined> {
+		let channel = await this.createTextChannel(message)
+		if(!channel) return undefined
+		channel.send(`<@${message.author.id}>, please follow the survey to set up your event.`)
+		channel.send('Send "abort" if you want to abort event setup process')
+
+		let options = {} as EventOptions
+		let filter: (m: Message) => boolean = m => m.author.id === message.author.id
+
+		let game = await this.getDetail(channel, this.eventQuestions[0], filter)
+		.then((game) => {
+			return !game ? this.getDetail(channel as TextChannel, this.eventQuestions[0], filter) : game
+		})
+
+		if(!game) {
+			channel.send('Event was not set up')
+			channel.delete()
+			return undefined
+		}
+		if(game === "abort") {
+		channel.delete()
+		return undefined
+		}
+
+		let max = 3
+		let index = 0
+		let when: string | undefined, description: string | undefined
+		while (index < max) {
+			when = await this.getDetail(channel, this.eventQuestions[1], m => m.author.id === message.author.id)
+			if(when === "abort") {
+				channel.delete()
+				return undefined
+			}
+			description = await this.getDetail(channel, this.eventQuestions[2], m => m.author.id === message.author.id)
+			if(description === "abort") {
+				channel.delete()
+				return undefined
+			}
+			if (!when && !description) {
+				channel.send("Either 'when' or 'description' should be filled.")
+				++index
+			}
+			else break
+		}
+		if (!when && !description) {
+			channel.send("Event was not set up")
+			channel.delete()
+			return undefined
+		}
+
+		options.game = game
+		if (when) options.when = when
+		if (description) options.description = description
+
+		channel.delete()
+
+		return options
+	}
+
+	private async getDetail(channel: TextChannel, step: SetupStep, filter: (m: Message) => boolean): Promise<string | undefined> {
+		let result: string | undefined
+		return channel.send(step.question)
+			.then(async () => {
+				try {
+					const collected = await channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] });
+					let response = collected.first()?.content;
+					if(response === "abort") return response
+					if (!step.required || step.validate(response)) {
+						result =  step.skipped(response) ? undefined : response;
+					}
+					else {
+						channel.send(step.warning);
+					}
+
+					return result;
+				}
+				catch (e) {
+					channel.send('Took to long to respond.');
+					return result;
+				}
+			});
+	}
+    
+    
+
+	private async createTextChannel(message: Message): Promise<TextChannel | undefined> {
+		let user = message.author
+		let channel = message.channel as GuildChannel
+		let guild = message.guild
+		if (guild) {
+			let options: GuildCreateChannelOptions = {
+				permissionOverwrites: [{ id: guild.id, deny: ['VIEW_CHANNEL'] }],
+                type: "text",
+                parent: channel.parent?.id
+			}
+			if (channel) {
+				return guild.channels.create('lfg-setup', options)
+					.then(ch => {
+						ch.overwritePermissions([
+							{
+								id: user ? user.id : "undefined",
+								allow: ['VIEW_CHANNEL'],
+							},
+						]);
+						return ch as TextChannel;
+					});
+			}
+		}
+
+		return undefined
+	}
+}
--- src/handlers/LfgMessageHandlers.ts
+++ src/handlers/LfgMessageHandlers.ts
@@ -4,22 +4,19 @@ import { Config } from "../config";
 import { LfgChannel } from "../entities/LfgChannel";
 import { BotCommand } from "../enums/BotCommand";
 import { EventOptions } from "../entities/EventOptions";
-import { SetupStep } from "../entities/SetupStep";
+import { EventSetupHandler } from "./EventSetupHandler";
 
 
 
 export class LfgMessageHandlers {
 	private mongoConnector: MongoConnector
 	private config: Config
-	private eventQuestions: SetupStep[] = [
-		new SetupStep('What game would you like to play?', 'Game should be filled.', (response) => { return response && response !== "skip" ? true : false }, true),
-		new SetupStep('When? (send "skip" if you don\'t want to fill the field)', 'Error processing your response', (response) => { return response ? true : false }, false),
-		new SetupStep('How would you describe this event? (send "skip" if you don\'t want to fill the field)', 'Error processing your response', (response) => { return response ? true : false }, false)
-	]
+	private eventSetupHandler: EventSetupHandler
 
 	constructor(mongoConnector: MongoConnector, config: Config) {
 		this.mongoConnector = mongoConnector
 		this.config = config
+		this.eventSetupHandler = new EventSetupHandler()
 	}
 
 	public async validateReaction(reaction: MessageReaction) {
@@ -58,7 +55,7 @@ export class LfgMessageHandlers {
 		let channel = message.channel
 		let author = message.author
 
-		this.setupEvent(message)
+		this.eventSetupHandler.setupEvent(message)
 			.then((options: EventOptions | undefined) => {
 				if (options) {
 					let embed = this.createEmbed(author, options)
@@ -83,72 +80,5 @@ export class LfgMessageHandlers {
 		if (options.when) embed.addField("**When**", options.when, true)
 
 		return embed
-	}
-
-	private async setupEvent(message: Message): Promise<EventOptions | undefined> {
-		let dmChannel = await message.author.createDM()
-		dmChannel.send('Send "abort" if you want to abort event setup process')
-
-		let options = {} as EventOptions
-
-		let game = await this.getDetail(dmChannel, this.eventQuestions[0])
-		.then((game) => {
-			return !game ? this.getDetail(dmChannel, this.eventQuestions[0]) : game
-		})
-
-		if(!game) {
-			dmChannel.send('Event was not set up')
-			return undefined
-		}
-		if(game === "abort") return undefined
-
-		let max = 3
-		let index = 0
-		let when: string | undefined, description: string | undefined
-		while (index < max) {
-			when = await this.getDetail(dmChannel, this.eventQuestions[1])
-			if(when === "abort") return undefined
-			description = await this.getDetail(dmChannel, this.eventQuestions[2])
-			if(description === "abort") return undefined
-			if (!when && !description) {
-				dmChannel.send("Either 'when' or 'description' should be filled.")
-				++index
-			}
-			else break
-		}
-		if (!when && !description) {
-			dmChannel.send("Event was not set up")
-			return undefined
-		}
-
-		options.game = game
-		if (when) options.when = when
-		if (description) options.description = description
-
-		return options
-	}
-
-	private async getDetail(channel: DMChannel, step: SetupStep): Promise<string | undefined> {
-		let result: string | undefined
-		return channel.send(step.question)
-			.then(async () => {
-				try {
-					const collected = await channel.awaitMessages(() => { return true; }, { max: 1, time: 30000, errors: ['time'] });
-					let response = collected.first()?.content;
-					if(response === "abort") return response
-					if (!step.required || step.validate(response)) {
-						result =  step.skipped(response) ? undefined : response;
-					}
-					else {
-						channel.send(step.warning);
-					}
-
-					return result;
-				}
-				catch (e) {
-					channel.send('Took to long to respond.');
-					return result;
-				}
-			});
 	}
 }
