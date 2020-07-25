9ff5e33c62579bab2257fc4761cf8a70be9f4c08
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-07-25 18:02:57 +0300

    update main.ts for Glitch

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 'use strict'
 
 import { Bot } from './discord'
-import * as express from "express";
-import * as https from 'https';
+import { application } from "express";
 
 const client: Bot = new Bot()
 client.start()
 
-const app = express();
-app.get("/", (request, response) => {
+application.get("/", (request, response) => {
     console.log(Date.now() + " Ping Received");
     response.sendStatus(200);
   });
-  app.listen(process.env.PORT);
-  setInterval(() => {
-    https.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-  }, 280000);
