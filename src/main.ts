f9a3cef0de22a2af7296cd4139c93d09f89f961d
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-07-25 16:39:53 +0300

    add self-ping for Glitch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 'use strict'
 
 import { Bot } from './discord'
+import * as express from "express";
+import * as https from 'https';
 
 const client: Bot = new Bot()
 client.start()
+
+const app = express();
+app.get("/", (request, response) => {
+    console.log(Date.now() + " Ping Received");
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT);
+  setInterval(() => {
+    https.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  }, 280000);
